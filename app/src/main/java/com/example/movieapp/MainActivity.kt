51321887fddbaa4015650c26c73094e2dcc941f4
gultencelikbilek <81778970+gultencelikbilek@users.noreply.github.com>
2024-04-16 21:01:52 +0300

    Compoleted  Movie List screen

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.movieapp.presentation.home_screen.HomeScreen
 import com.example.movieapp.presentation.movie_list_screen.MovieListScreen
@@ -24,21 +28,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MovieAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Text(text = "${viewmodel.state.movies}")
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.linearGradient(
+                            listOf(
+                                Color(0xFFB226E1),
+                                Color(0xFFFC6603),
+                                Color(0xFF5995EE),
+                                Color(0xFF3D3535)
+                            )
+                        )
+                    )){
+                    MovieListScreen()
                 }
+
+             //   Surface(
+             //       modifier = Modifier.fillMaxSize(),
+             //       color = MaterialTheme.colorScheme.background
+             //   ) {
+             //       Text(text = "${viewmodel.state.movies}")
+             //   }
             }
         }
-    }
-}
-@Preview
-@Composable
-fun Hello() {
-    MovieAppTheme {
-      MovieListScreen()
     }
 }
--- app/src/main/java/com/example/movieapp/presentation/movie_list_screen/MovieListScreen.kt
+++ app/src/main/java/com/example/movieapp/presentation/movie_list_screen/MovieListScreen.kt
@@ -1,49 +1,178 @@
 package com.example.movieapp.presentation.movie_list_screen
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
+import com.example.movieapp.domain.model.Data
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun MovieListScreen() {
+fun MovieListScreen(
 
+) {
+
+    val movieListViewModel: MovieListViewModel = hiltViewModel()
+    val state = movieListViewModel.state
     Scaffold(
+        modifier = Modifier.background(Color.Transparent),
         topBar = {
             TopBarHeader()
-        }
+        },
+        content = { paddingValues ->
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                Modifier
+                    .padding(paddingValues)
+                    .fillMaxSize()
+                    .background(Color.Transparent),
+                content = {
+                    items(state.movies.size) {
+                        MovieItemUi(itemIndex = it, movieList = state.movies)
+                    }
+                }
+            )
+        },
+        containerColor = Color.Transparent
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MovieItemUi(itemIndex: Int, movieList: List<Data>) {
+    Card(
+        Modifier
+            .wrapContentSize()
+            .padding(10.dp)
+            .clickable { },
+        elevation = CardDefaults.cardElevation(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Blue,
+            contentColor = Color.Red
+        )
     ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+
+            AsyncImage(
+                model = movieList[itemIndex].poster,
+                contentDescription = movieList[itemIndex].title,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(10.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.LightGray.copy(.7f))
+                    .padding(6.dp)
+            ) {
+                Text(
+                    text = movieList[itemIndex].title,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .basicMarquee(),
+                    textAlign = TextAlign.Center,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    style = TextStyle(
+                        shadow = Shadow(
+                            Color(0xFFFC6603), offset = Offset(1f, 1f), 3f
+
+                        )
+                    )
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    modifier = Modifier.align(Alignment.End)
+                ) {
+                    Icon(imageVector = Icons.Rounded.Star, contentDescription = "star")
+                    Spacer(modifier = Modifier.width(15.dp))
+                    Text(
+                        text = movieList[itemIndex].imdb_rating,
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 8.dp),
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        maxLines = 1
+                    )
+                }
+
+            }
+
+        }
 
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBarHeader() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 14.dp),
-        horizontalArrangement = Arrangement.Center
-
-    ) {
-        Text(
-            text = "Movie App",
-            style = TextStyle(
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
+    TopAppBar(
+        title = {
+            Text(
+                text = "Movie App",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
             )
-        )
-
-    }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.White.copy(0.4f)
+        ),
+        modifier = Modifier.padding(start = 120.dp,end = 110.dp)
+    )
 
 }
